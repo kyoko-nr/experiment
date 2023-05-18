2552dead04fc16b3896b044228d407a81eeee3ab
kyoko-nr <kyokokitagawa@me.com>
2023-05-18 09:25:59 +0900

    sassの導入、snap、bgColorのアニメーション追加

--- 01_text/src/scripts/shawshank.ts
+++ 01_text/src/scripts/shawshank.ts
@@ -1,4 +1,4 @@
-import { gsap } from "gsap";
+import { gsap, Expo } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
@@ -7,14 +7,34 @@ const texts = document.querySelectorAll(".bl_shawshank_wheelText");
 const block = document.querySelector(".bl_shawshank");
 
 export const initShawsank = () => {
-  gsap.to(texts, {
-    rotation: "+=180",
-    ease: "none",
+  const tl = gsap.timeline({
     scrollTrigger: {
       trigger: block,
       scrub: 1,
       pin: true,
-      markers: true
+      markers: true,
+      snap: {
+        snapTo: 0.5,
+        duration: 1,
+        ease: Expo.easeOut
+      }
     }
   });
+
+  tl.to(
+    texts,
+    {
+      rotation: "+=180",
+      ease: "none"
+    },
+    0
+  );
+
+  tl.to(
+    block,
+    {
+      backgroundColor: "#0086AB"
+    },
+    0
+  );
 };
