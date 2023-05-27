5444b74ec69ded9932fc40adc279ee43c5da874a
kyoko-nr <kyokokitagawa@me.com>
2023-05-27 14:20:52 +0900

    blobでlottieのspriteをマスクする、blobでinterpolatorを使う

--- 02_interpolate/src/scripts/main.ts
+++ 02_interpolate/src/scripts/main.ts
@@ -3,17 +3,7 @@ import jsondata from "../images/lottie/lottie-anim-1.json?raw";
 import { createPixiApp } from "./pixi/createPixiApp";
 import { loadAnimation } from "./lottie/loadAnimation";
 import { createSprite } from "./pixi/createSprite";
-import { Graphics } from "pixi.js";
-
-const getCircle = () => {
-  const circle = new Graphics();
-  circle.clear();
-  circle.beginFill(0x00ff00);
-  circle.drawCircle(0, 0, 100);
-  circle.endFill();
-  circle.pivot.set(0, 0);
-  return circle;
-};
+import { Blob } from "./pixi/class/Blob";
 
 const init = () => {
   const div = document.querySelector<HTMLDivElement>("#app");
@@ -32,10 +22,13 @@ const init = () => {
 
   app.stage.addChild(sprite);
 
-  const circle = getCircle();
-  circle.position.x = window.innerWidth / 2;
-  circle.position.y = window.innerHeight / 2;
-  app.stage.addChild(circle);
+  const blob = new Blob();
+  blob.position.x = window.innerWidth / 2;
+  blob.position.y = window.innerHeight / 2;
+  app.stage.addChild(blob);
+
+  // マスク
+  sprite.mask = blob;
 
   div.appendChild(app.view);
 
--- 02_interpolate/src/scripts/pixi/class/Blob.ts
+++ 02_interpolate/src/scripts/pixi/class/Blob.ts
@@ -1,19 +1,35 @@
-import { Graphics } from "pixi.js";
+import { Graphics, Point } from "pixi.js";
 import { CurveInterpolator } from "curve-interpolator";
 
 export class Blob extends Graphics {
   private readonly blobName = "blob";
 
+  radius = 400;
+  count = 8;
+  polAngle = (Math.PI * 2) / this.count;
+  points: Point[] = [];
+
   constructor () {
     super();
-
+    for (let i = 0; i < this.count; i++) {
+      const x = Math.cos(i * this.polAngle) * this.radius;
+      const y = Math.sin(i * this.polAngle) * this.radius;
+      this.points.push(new Point(x, y));
+    }
+    this.pivot.set(0, 0);
     this.draw();
   }
 
   draw () {
+    const interp = new CurveInterpolator(
+      this.points.map((p) => [p.x, p.y]),
+      { tension: 0, closed: true }
+    );
+    const ps = interp.getPoints(40) as Array<[number, number]>;
+    const newp = Array.from(ps).map(([x, y]) => new Point(x, y));
     this.clear();
-    this.beginFill(0x00ff00);
-    this.drawCircle(0, 0, 100);
+    this.beginFill(0x00ffff);
+    this.drawPolygon(newp);
     this.endFill();
   }
 
