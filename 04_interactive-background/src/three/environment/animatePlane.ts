66e19f75bcd932eba77b4ccda4c2398d41febec5
kyoko-nr <kyoko_kitagawa@outlook.jp>
2024-02-23 16:03:51 +0900

    update animation of plane

--- 04_interactive-background/src/three/environment/animatePlane.ts
+++ 04_interactive-background/src/three/environment/animatePlane.ts
@@ -4,25 +4,23 @@ import { getGui } from "../gui/gui";
 
 const noise = new SimplexNoise();
 const animConfs = {
-  allVolume: 1.5,
-  xVolume: 1,
-  yVolume: 1
+  height: 1.5,
+  xVolume: 5,
+  yVolume: 5,
+  speed: 0.0001,
 }
 
 export const animatePlane = (plane: THREE.Mesh) => {
   const position = plane.geometry.getAttribute("position");
 
   for(let i = 0; i < position.count; i++) {
-    const time = Date.now() * 0.0001;
+    const time = Date.now() * animConfs.speed;
     const x = position.getX(i);
     const y = position.getY(i);
 
-    const temp = noise.noise(
-      x * animConfs.xVolume + time,
-      y * animConfs.yVolume + time
-      ) * animConfs.allVolume;
+    const n = noise.noise3d(x / animConfs.xVolume, y/animConfs.yVolume, time) * animConfs.height;
 
-    position.setZ(i, temp);
+    position.setZ(i, n);
   }
   position.needsUpdate = true;
 }
@@ -30,9 +28,10 @@ export const animatePlane = (plane: THREE.Mesh) => {
 const createGui = () => {
   const gui = getGui();
   const folder = gui.addFolder("animation");
-  folder.add(animConfs, "allVolume", 0, 10, 0.1);
+  folder.add(animConfs, "height", 0, 10, 0.1);
   folder.add(animConfs, "xVolume", 0, 10, 0.1);
   folder.add(animConfs, "yVolume", 0, 10, 0.1);
+  folder.add(animConfs, "speed", 0.00001, 0.001, 0.00001);
 }
 
 createGui();
