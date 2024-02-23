87b6864754cba60605758c1f8cddb6d637e88f68
kyoko-nr <kyoko_kitagawa@outlook.jp>
2024-02-23 16:34:50 +0900

    modify lights

--- 04_interactive-background/src/three/environment/createLights.ts
+++ 04_interactive-background/src/three/environment/createLights.ts
@@ -3,25 +3,22 @@ import { getGui } from "../gui/gui";
 
 const baseConf = {
   color: 0x0E09DC,
-  intensity: 1.5,
-  x: 10,
-  y: 10,
-  z: 30
+  intensity: 0.9,
+  x: 0,
+  y: 8,
+  z: 16
 }
 
 const lightConfs = [
   baseConf,
-  {...baseConf, color: 0x1CD1E1, x: -10, y: 10, z: 30},
-  {...baseConf, color: 0x18C02C, x: 30, y: 30, z: 20},
-  // {...baseConf, color: 0xee3bcf, x: -30, y: -10, z: 20}
+  {...baseConf, color: 0x1CD1E1, x: 23, y: 18, z: 10},
+  {...baseConf, color: 0xffadf0, x: 26, y: 10, z: -2},
 ]
 
 export const createLights = () => {
   const lights: THREE.Object3D[] = [];
   const helpers: THREE.Object3D[] = []
   for(const conf of lightConfs) {
-    // const light = new THREE.PointLight(conf.color, conf.intensity, 1000);
-    // const helper = new THREE.PointLightHelper(light, 3)
     const light = new THREE.DirectionalLight(conf.color, conf.intensity);
     const helper = new THREE.DirectionalLightHelper(light, 3);
     light.position.set(conf.x, conf.y, conf.z);
@@ -31,7 +28,9 @@ export const createLights = () => {
   }
   createGui(lights)
 
-  return [...lights, ...helpers];
+  const ambLight = new THREE.AmbientLight(0xffffff, 0.3);
+
+  return [...lights, ...helpers, ambLight];
 }
 
 const createGui = (lights: THREE.Object3D[]) => {
@@ -39,8 +38,8 @@ const createGui = (lights: THREE.Object3D[]) => {
   for(const [index, light] of Object.entries(lights)) {
     const folder = gui.addFolder(`light${(Number(index) + 1)}`);
     folder.addColor(light, "color");
-    folder.add(light.position, "x", -500, 500, 1);
-    folder.add(light.position, "y", -500, 500, 1);
-    folder.add(light.position, "z", -500, 500, 1);
+    folder.add(light.position, "x", -100, 100, 0.1);
+    folder.add(light.position, "y", -100, 100, 0.1);
+    folder.add(light.position, "z", -100, 100, 0.1);
   }
 }
