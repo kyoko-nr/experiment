05f90d8a3e28b8674ce5e508f2770c89606231bf
kyoko-nr <kyoko_kitagawa@outlook.jp>
2024-05-06 17:22:17 +0900

    remove unused files, add three.js scene

---
+++ 05_webgl-scroll/src/three/initThree.ts
@@ -0,0 +1,49 @@
+import {
+  WebGLRenderer,
+  PerspectiveCamera,
+  Scene,
+  BoxGeometry,
+  MeshNormalMaterial,
+  Mesh,
+} from "three";
+
+const FOV = 60;
+
+const createRenderer = () => {
+  const renderer = new WebGLRenderer();
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  return renderer;
+};
+
+const createCamera = () => {
+  const fov = FOV;
+  const fovRad = (fov / 2) * (Math.PI / 180);
+  const dist = window.innerHeight / 2 / Math.tan(fovRad);
+  const camera = new PerspectiveCamera(
+    fov,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000,
+  );
+  camera.position.z = dist;
+  return camera;
+};
+
+const createScene = () => {
+  return new Scene();
+};
+
+export const initThree = () => {
+  const renderer = createRenderer();
+  const camera = createCamera();
+  const scene = createScene();
+
+  const geo = new BoxGeometry(100, 100, 100);
+  const mat = new MeshNormalMaterial();
+  const box = new Mesh(geo, mat);
+
+  scene.add(box);
+
+  return { renderer, camera, scene };
+};
