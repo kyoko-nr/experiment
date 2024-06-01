1d334b8b379eefb6a98d891764e3f98276ff6fea
kyoko-nr <kyoko_kitagawa@outlook.jp>
2024-06-01 11:00:48 +0900

    update vertex shader for wave animation

--- 05_webgl-scroll/src/three/shaders/vShader.glsl
+++ 05_webgl-scroll/src/three/shaders/vShader.glsl
@@ -1,18 +1,16 @@
 varying vec2 vUv;
 uniform float uTime;
 
-float PI = 3.1415926535897932384626433832795;
-
 void main()
 {
   vUv = uv;
 
   vec3 pos = position;
 
-  float offset = 0.01;
-  float freq = 1.0;
-  float amp = 0.1;
-  float x = pos.x + sin(pos.y * offset + uTime * freq * PI) * amp;
+  float speed = 3.0;
+  float freq = 4.0;
+  float amp = 0.02;
+  float x = pos.x + sin((uTime * speed) + (pos.y * freq)) * amp;
 
   gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4( vec3(x, pos.y, pos.z), 1.0);
 }
