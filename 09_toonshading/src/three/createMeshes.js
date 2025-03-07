82d42e0e6a5dad91d394d481ea5c76e05fa6500b
kyoko-nr <kyokokitagawa@me.com>
2025-03-07 09:15:26 +0900

    added lil-gui, three.js scene

---
+++ 09_toonshading/src/three/createMeshes.js
@@ -0,0 +1,39 @@
+import * as THREE from "three";
+
+/**
+ * Create meshes
+ * @returns {THREE.Group}
+ */
+export const createMeshes = () => {
+  const group = new THREE.Group();
+
+  const material = new THREE.MeshNormalMaterial({});
+
+  // Torus knot
+  const torusKnot = new THREE.Mesh(
+    new THREE.TorusKnotGeometry(0.6, 0.25, 128, 32),
+    material
+  );
+  torusKnot.position.x = 3
+  torusKnot.name = 'torusKnot';
+  group.add(torusKnot);
+
+  // Sphere
+  const sphere = new THREE.Mesh(
+    new THREE.SphereGeometry(),
+    material
+  );
+  sphere.position.x = - 3;
+  sphere.name = 'sphere';
+  group.add(sphere);
+
+  // Cube
+  const cube = new THREE.Mesh(
+    new THREE.IcosahedronGeometry(1, 1),
+    material
+  );
+  cube.name = 'cube';
+  group.add(cube);
+
+  return group;
+}
---
+++ 09_toonshading/src/three/initThree.js
@@ -0,0 +1,90 @@
+import * as THREE from "three";
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
+import GUI from "lil-gui";
+import { createMeshes } from "./createMeshes";
+
+const SIZE = {
+  width: 0,
+  height: 0,
+  dpr: 0,
+};
+
+const rendererParams = {
+  clearColor: "#d5d1b3",
+}
+
+/**
+ * Initiate Three.js
+ * @param {HTMLDivElement} app
+ */
+export const initThree = (app) => {
+  const { scene, camera, renderer } = createEnvironment();
+
+  const controls = new OrbitControls(camera, renderer.domElement);
+  controls.enableDamping = true;
+
+  const meshes  = createMeshes();
+  scene.add(meshes);
+
+  app.appendChild(renderer.domElement);
+  
+  // const clock = new THREE.Clock();
+  
+  addGui(renderer);
+
+  const tick = () => {
+    renderer.render(scene, camera);
+    window.requestAnimationFrame(tick);
+  };
+  tick();
+
+  window.addEventListener("resize", () => {
+    onResize(camera, composer, renderer);
+  });
+};
+
+const createEnvironment = () => {
+  SIZE.width = window.innerWidth;
+  SIZE.height = window.innerHeight;
+
+  const scene = new THREE.Scene();
+
+  const camera = new THREE.PerspectiveCamera(
+    60,
+    SIZE.width / SIZE.height,
+    0.1,
+    10
+  );
+  camera.position.set(0, 0, 5);
+
+  const renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setSize(SIZE.width, SIZE.height);
+  SIZE.dpr = Math.min(window.devicePixelRatio, 2);
+  renderer.setPixelRatio(SIZE.dpr);
+  renderer.setViewport(0, 0, SIZE.width, SIZE.height);
+  renderer.setClearColor(rendererParams.clearColor);
+
+  return { renderer, camera, scene };
+};
+
+const onResize = (camera, renderer) => {
+  SIZE.width = window.innerWidth;
+  SIZE.height = window.innerHeight;
+
+  camera.aspect = SIZE.width / SIZE.height;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(SIZE.width, SIZE.height);
+  SIZE.dpr = Math.min(window.devicePixelRatio, 2);
+  renderer.setPixelRatio(SIZE.dpr);
+  renderer.setViewport(0, 0, SIZE.width, SIZE.height);
+}
+
+//--------GUI--------
+const gui = new GUI();
+const addGui = (renderer) => {
+  gui.addColor(rendererParams, "clearColor")
+  .onChange(() => renderer.setClearColor(rendererParams.clearColor));
+
+}
+
