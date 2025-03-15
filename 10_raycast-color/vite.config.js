98de2c8f35b35d19ddfcb038243c1a3216f10da9
kyoko-nr <kyokokitagawa@me.com>
2025-03-15 10:03:16 +0900

    add vite config

---
+++ 10_raycast-color/vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import glsl from 'vite-plugin-glsl'
+
+export default defineConfig({
+  root: 'src/',
+  base: './',
+  server: {
+    host: true
+  },
+  plugins: [
+    glsl()
+  ],
+})
+
