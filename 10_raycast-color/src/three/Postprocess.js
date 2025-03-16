d319cea14238110a00737ab4bce7cd2541788309
kyoko-nr <kyokokitagawa@me.com>
2025-03-16 17:52:53 +0900

    delete gui of noise texture

--- 10_raycast-color/src/three/Postprocess.js
+++ 10_raycast-color/src/three/Postprocess.js
@@ -8,39 +8,13 @@ import { ShaderPass } from "three/examples/jsm/postprocessing/ShaderPass.js";
 import { getSize } from "../utils/getSize";
 import vertexShader from "./shaders/vertex.glsl";
 import fragmentShader from "./shaders/fragment.glsl";
-import noise1Src from "../assets/noise_1.png";
-import noise2Src from "../assets/noise_2.png";
-import noise3Src from "../assets/noise_3.png";
-import noise4Src from "../assets/noise_4.png";
-import noise5Src from "../assets/noise_5.png";
-import noise6Src from "../assets/noise_6.png";
+import noiseSrc from "../assets/noise_4.png";
 
 const loader = new THREE.TextureLoader();
 
-const noise1 = loader.load(noise1Src);
-noise1.wrapS = THREE.RepeatWrapping;
-noise1.wrapT = THREE.RepeatWrapping;
-const noise2 = loader.load(noise2Src);
-noise2.wrapS = THREE.RepeatWrapping;
-noise2.wrapT = THREE.RepeatWrapping;
-const noise3 = loader.load(noise3Src);
-noise3.wrapS = THREE.RepeatWrapping;
-noise3.wrapT = THREE.RepeatWrapping;
-const noise4 = loader.load(noise4Src);
-noise4.wrapS = THREE.RepeatWrapping;
-noise4.wrapT = THREE.RepeatWrapping;
-const noise5 = loader.load(noise5Src);
-noise5.wrapS = THREE.RepeatWrapping;
-noise5.wrapT = THREE.RepeatWrapping;
-const noise6 = loader.load(noise6Src);
-noise6.wrapS = THREE.RepeatWrapping;
-noise6.wrapT = THREE.RepeatWrapping;
-
-const Noises = {"noise1": noise1, "noise2": noise2, "noise3": noise3, "noise4": noise4, "noise5": noise5, "noise6": noise6};
-
-const postprocessParams = {
-  noise: noise4,
-}
+const noise = loader.load(noiseSrc);
+noise.wrapS = THREE.RepeatWrapping;
+noise.wrapT = THREE.RepeatWrapping;
 
 /**
  * Post process effect.
@@ -67,9 +41,6 @@ export class Postprocess {
     this.environment.addMesh(this.displacement.plane);
 
     this.setupColorEffect();
-
-    // ----------GUI----------
-    this.addGui();
   }
 
   setupColorEffect() {
@@ -80,7 +51,7 @@ export class Postprocess {
       uniforms: {
         uTexture: new THREE.Uniform(this.displacement.texture),
         tDiffuse: new THREE.Uniform(null),
-        uNoise: new THREE.Uniform(postprocessParams.noise),
+        uNoise: new THREE.Uniform(noise),
         uTime: new THREE.Uniform(0),
       }
     }
@@ -111,14 +82,5 @@ export class Postprocess {
     this.composer.setSize(size.width, size.height);
     this.composer.setPixelRatio(size.dpr);
     this.displacement.onResize(this.environment.camera);
-  }
-
-  addGui() {
-    const folder = gui.addFolder("Postprocess");
-    folder.add(postprocessParams, "noise", Noises).onChange(
-      (value) => {
-        this.colorPass.uniforms.uNoise.value = value;
-      }
-    );
   }
 }
