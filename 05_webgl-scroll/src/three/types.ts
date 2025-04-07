9a98ebef6de31fb76dcae1de8df1dc909e5ee612
kyoko-nr <kyokokitagawa@me.com>
2025-04-07 20:50:12 +0900

    delete not working project

--- 05_webgl-scroll/src/three/types.ts
+++
@@ -1,9 +0,0 @@
-import { Mesh } from "three";
-
-/**
- * Type of ImagePlane
- */
-export type ImagePlane = {
-  dom: HTMLImageElement;
-  mesh: Mesh;
-};
--- 05_webgl-scroll/src/utils/easing.ts
+++
@@ -1,9 +0,0 @@
-/**
- * lerp
- * @param start start point
- * @param end end point
- * @param multiplier amount of ease
- */
-export const lerp = (start: number, end: number, multiplier: number) => {
-  return (1 - multiplier) * start + multiplier * end;
-};
