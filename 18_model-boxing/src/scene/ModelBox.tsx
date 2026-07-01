c060345599c1f69c3c9bfb069e4cb2fba20cb505
kyoko-nr <kyokokitagawa@me.com>
2026-07-01 21:40:48 +0900

    不要なstateの削除、モデルをboxに分割

--- 18_model-boxing/src/scene/ModelBox.tsx
+++ 18_model-boxing/src/scene/ModelBox.tsx
@@ -7,22 +7,25 @@ import {
   TorusKnotGeometry,
   type Node,
 } from "three/webgpu";
-import {
-  boxRotationSpeedAtom,
-  boxScaleAtom,
-  shaderMixAtom,
-  wireframeAtom,
-} from "../state/sceneAtoms";
+import { boxDivision, shaderMixAtom, wireframeAtom } from "../state/sceneAtoms";
 import { createBoxColorNode } from "../shaders/createBoxColorNode";
 
 export const ModelBox = () => {
   const meshRef = useRef<Mesh>(null);
-  const rotationSpeed = useAtomValue(boxRotationSpeedAtom);
-  const scale = useAtomValue(boxScaleAtom);
   const shaderMix = useAtomValue(shaderMixAtom);
   const wireframe = useAtomValue(wireframeAtom);
+  const division = useAtomValue(boxDivision);
 
-  const geometry = useMemo(() => new TorusKnotGeometry(0.5, 0.2, 128), []);
+  const geometry = useMemo(() => {
+    const geom = new TorusKnotGeometry(0.5, 0.2, 128);
+    const posArry = geom.attributes.position.array;
+    for (let i = 0; i < posArry.length; i++) {
+      posArry[i] = Math.round(posArry[i] * division) / division;
+    }
+    geom.attributes.position.needsUpdate = true;
+    return geom;
+  }, [division]);
+
   const material = useMemo(() => {
     const nextMaterial = new MeshBasicNodeMaterial();
     nextMaterial.colorNode = createBoxColorNode(shaderMix) as Node<"vec3">;
@@ -39,16 +42,9 @@ export const ModelBox = () => {
 
   useFrame((_, delta) => {
     if (!meshRef.current) return;
-    meshRef.current.rotation.x += delta * rotationSpeed * 0.6;
-    meshRef.current.rotation.y += delta * rotationSpeed;
+    meshRef.current.rotation.x += delta * 0.3 * 0.6;
+    meshRef.current.rotation.y += delta * 0.3;
   });
 
-  return (
-    <mesh
-      ref={meshRef}
-      geometry={geometry}
-      material={material}
-      scale={[scale, scale, scale]}
-    />
-  );
+  return <mesh ref={meshRef} geometry={geometry} material={material} />;
 };
--- 18_model-boxing/src/state/sceneAtoms.ts
+++ 18_model-boxing/src/state/sceneAtoms.ts
@@ -1,6 +1,6 @@
 import { atom } from "jotai";
 
-export const boxRotationSpeedAtom = atom(0.35);
-export const boxScaleAtom = atom(1);
 export const shaderMixAtom = atom(0.45);
 export const wireframeAtom = atom(false);
+/** モデルを分割する数 */
+export const boxDivision = atom(24);
