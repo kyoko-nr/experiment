274f68ba9a8a6656ae645d9cc71e7076be07bdce
kyoko-nr <kyokokitagawa@me.com>
2026-07-01 21:43:16 +0900

    不要なcontrolの削除

--- 18_model-boxing/src/controls/LevaControls.tsx
+++ 18_model-boxing/src/controls/LevaControls.tsx
@@ -1,24 +1,21 @@
 import { Leva, useControls } from "leva";
 import { useSetAtom } from "jotai";
 import { useEffect } from "react";
-import { boxDivision, shaderMixAtom, wireframeAtom } from "../state/sceneAtoms";
+import { boxDivision, wireframeAtom } from "../state/sceneAtoms";
 
 export const LevaControls = () => {
-  const setShaderMix = useSetAtom(shaderMixAtom);
   const setWireframe = useSetAtom(wireframeAtom);
   const setDivision = useSetAtom(boxDivision);
 
   const controls = useControls("Boxing model", {
-    shaderMix: { value: 0.45, min: 0, max: 1, step: 0.01 },
     wireframe: false,
-    division: { value: 24, min: 2, max: 100, step: 1 },
+    division: { value: 5, min: 2, max: 100, step: 1 },
   });
 
   useEffect(() => {
-    setShaderMix(controls.shaderMix);
     setWireframe(controls.wireframe);
     setDivision(controls.division);
-  }, [controls, setShaderMix, setWireframe, setDivision]);
+  }, [controls, setWireframe, setDivision]);
 
   return <Leva collapsed={false} />;
 };
--- 18_model-boxing/src/state/sceneAtoms.ts
+++ 18_model-boxing/src/state/sceneAtoms.ts
@@ -3,4 +3,4 @@ import { atom } from "jotai";
 export const shaderMixAtom = atom(0.45);
 export const wireframeAtom = atom(false);
 /** モデルを分割する数 */
-export const boxDivision = atom(24);
+export const boxDivision = atom(5);
